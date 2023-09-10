e38ae497a146f9035ca82775431154a7970b4ec5
MD ASIFUL ALAM <66010854+axif0@users.noreply.github.com>
2023-09-09 23:39:44 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import ContactForm from './CgKnow/ContactForm'
+
+export default function App() {
+  return (
+    <div>
+        <ContactForm/>
+    </div>
+  )
+}
+
+
+
