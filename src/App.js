764666097f6d1f2459a96a6ac68f779f69e8e17c
MD ASIFUL ALAM <66010854+axif0@users.noreply.github.com>
2023-10-02 21:56:02 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import Con from './testing/test';
+// import Con from './testing/test';
 // import Feture from './testing/feature';
 import Home from './testing/home';
+import Manual from './testing/manual';
  
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/questions" element={<Con/>} />
+          <Route path="/questions" element={<Manual/>} />
           {/* <Route path="/" element={<Feture/>} /> */}
           <Route path="/" element={<Home/>} />
         
